637b7822951b13c3f5f6c991a4ea3bcaea1b0794
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2021-03-17 17:10:45 +0530

    finished 3_10

--- 03_10/Backpack.js
+++ 03_10/Backpack.js
@@ -5,36 +5,36 @@
  * Class expression:  const Name = class {}
  */
 
-class Backpack {
-  constructor(
-    // Defines parameters:
-    name,
-    volume,
-    color,
-    pocketNum,
-    strapLengthL,
-    strapLengthR,
-    lidOpen
-  ) {
-    // Define properties:
-    this.name = name;
-    this.volume = volume;
-    this.color = color;
-    this.pocketNum = pocketNum;
-    this.strapLength = {
-      left: strapLengthL,
-      right: strapLengthR,
-    };
-    this.lidOpen = lidOpen;
-  }
-  // Add methods like normal functions:
-  toggleLid(lidStatus) {
-    this.lidOpen = lidStatus;
-  }
-  newStrapLength(lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  }
-}
+const Backpack = class {
+	constructor(
+		// Defines parameters:
+		name,
+		volume,
+		color,
+		pocketNum,
+		strapLengthL,
+		strapLengthR,
+		lidOpen
+	) {
+		// Define properties:
+		this.name = name;
+		this.volume = volume;
+		this.color = color;
+		this.pocketNum = pocketNum;
+		this.strapLength = {
+			left: strapLengthL,
+			right: strapLengthR,
+		};
+		this.lidOpen = lidOpen;
+	}
+	// Add methods like normal functions:
+	toggleLid(lidStatus) {
+		this.lidOpen = lidStatus;
+	}
+	newStrapLength(lengthLeft, lengthRight) {
+		this.strapLength.left = lengthLeft;
+		this.strapLength.right = lengthRight;
+	}
+};
 
 export default Backpack;
