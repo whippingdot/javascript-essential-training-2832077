0ad0403f9f245733436eee400985c81497ebb478
Sanjaay R <64354499+whippingdot@users.noreply.github.com>
2021-05-17 12:17:31 +0530

    finished the challenge

---
+++ Chapter 3/03_14/Book.js
@@ -0,0 +1,34 @@
+const Book = class {
+  constructor(
+    title,
+    author,
+    genre,
+    numberOfPages,
+    hasPictures,
+    // In object bookInfo
+    numberOfMainCharacters,
+    blurb,
+    sequelsOrPrequels
+  ) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.numberOfPages = numberOfPages;
+    this.hasPictures = hasPictures;
+    this.finished = false;
+    this.bookInfo = {
+      mainCharCount: numberOfMainCharacters,
+      hasBlurb: blurb,
+      inSeries: sequelsOrPrequels,
+    };
+  }
+  toggleFinished() {
+    if (this.finished == false) {
+      this.finished = true;
+    } else {
+      this.finished = false;
+    }
+  }
+};
+
+export default Book;
--- Chapter 3/03_14/script.js
+++ Chapter 3/03_14/script.js
@@ -1,11 +1,12 @@
 /**
  * Challenge: Create a new object type
- * 
+ *
  * - Create a new object type "Book" using a class or an object constructor function.
  * - Add at least 5 book objects.
  */
 
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -18,6 +19,83 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
-console.log("The everydayPack object:", everydayPack);
-console.log("The pocketNum value:", everydayPack.pocketNum);
-console.log("Days since aquired:", everydayPack.backpackAge());
+const percyJackson1 = new Book(
+  "The Lightning Thief",
+  "Rick Riordan",
+  "Fantasy",
+  377,
+  false,
+  3,
+  true,
+  true
+);
+
+percyJackson1.toggleFinished();
+
+const heroesOfOlympus1 = new Book(
+  "The Lost Hero",
+  "Rick Riordan",
+  "Fantasy",
+  557,
+  false,
+  3,
+  true,
+  true
+);
+
+heroesOfOlympus1.toggleFinished();
+
+const heroesOfOlympus2 = new Book(
+  "The Son of Neptune",
+  "Rick Riordan",
+  "Fantasy",
+  544,
+  false,
+  3,
+  true,
+  true
+);
+
+const magnusChase1 = new Book(
+  "The Sword of Summer",
+  "Rick Riordan",
+  "Fantasy",
+  513,
+  false,
+  4,
+  true,
+  true
+);
+
+magnusChase1.toggleFinished();
+
+const magnusChase2 = new Book(
+  "The Hammer of Thor",
+  "Rick Riordan",
+  "Fantasy",
+  528,
+  false,
+  5,
+  true,
+  true
+);
+
+magnusChase2.toggleFinished();
+
+const magnusChase3 = new Book(
+  "The Ship of the Dead",
+  "Rick Riordan",
+  "Fantasy",
+  432,
+  false,
+  3,
+  true,
+  true
+);
+
+console.log("The first book of Percy Jackson:", percyJackson1);
+console.log("The first book of The Heroes of Olympus:", heroesOfOlympus1);
+console.log("The second book of The Heroes of Olympus:", heroesOfOlympus2);
+console.log("The first book of Magnus Chase:", magnusChase1);
+console.log("The second book of Magnus Chase:", magnusChase2);
+console.log("The third book of Magnus Chase:", magnusChase3);
