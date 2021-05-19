eb8d1a1e758a22efa7ed38faf14c82d05308eff7
Sanjaay R <64354499+whippingdot@users.noreply.github.com>
2021-05-19 14:18:49 +0530

    small change

---
+++ Practice/05_04/script.js
@@ -0,0 +1,7 @@
+console.log(document.querySelector("body"));
+console.log(document.querySelector("div p"));
+console.log(document.querySelector(".feature.backpack__color"));
+console.log(document.querySelector(".feature.backpack__color span"));
+
+console.log(document.querySelectorAll("div p"));
+console.log(document.querySelectorAll(".feature.backpack__color"));
