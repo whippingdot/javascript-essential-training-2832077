4619fefce6660eed063de4ec284fc48dce4ac341
Sanjaay R <64354499+whippingdot@users.noreply.github.com>
2021-05-21 19:34:00 +0530

    EY

--- 05_10/script.js
+++ 05_10/script.js
@@ -1,10 +1,10 @@
 /**
  * Challenge: Add a new element
  * - In JavaScript, create a new element to hold a navigation menu
- * - Add an unordered list and a series of no less than five links to the list 
+ * - Add an unordered list and a series of no less than five links to the list
  * - Use single words like “home”, “about”, etc for the list items and set the src attribute to # for simplicity
  * - Add the new navigation element to the DOM directly after the header
- * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu. 
+ * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu.
  * - A tip: Use either display flex or display grid to create the horizontal menu.
  */
 
@@ -23,33 +23,31 @@ const everydayPack = new Backpack(
 );
 
 const content = `
-  
-    <figure class="backpack__image">
-      <img src=${everydayPack.image} alt="" />
-    </figure>
-    <h1 class="backpack__name">Everyday Backpack</h1>
-    <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
-      }</span></li>
-      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="packprop backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen
-      }</span></li>
-    </ul>
-  
+  <figure class="backpack__image">
+    <img src=${everydayPack.image} alt="" />
+  </figure>
+  <h1 class="backpack__name">Everyday Backpack</h1>
+  <ul class="backpack__features">
+    <li class="packprop backpack__volume">Volume:<span> ${
+      everydayPack.volume
+    }l</span></li>
+    <li class="packprop backpack__color">Color:<span> ${
+      everydayPack.color
+    }</span></li>
+    <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+    <li class="packprop backpack__pockets">Number of pockets:<span> ${
+      everydayPack.pocketNum
+    }</span></li>
+    <li class="packprop backpack__strap">Left strap length:<span> ${
+      everydayPack.strapLength.left
+    } inches</span></li>
+    <li class="packprop backpack__strap">Right strap length:<span> ${
+      everydayPack.strapLength.right
+    } inches</span></li>
+    <li class="packprop backpack__lid">Lid status:<span> ${
+      everydayPack.lidOpen
+    }</span></li>
+  </ul>
 `;
 
 const main = document.querySelector(".maincontent");
@@ -60,3 +58,25 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+// My Code
+
+const navBar = `
+  <ul class="sitenavbar">
+    <li><a href=#title>Home</a></li>
+    <li><a href=#slogan>Slogan</a></a></li>
+    <li><a href=#description>Description</a></li>
+    <li><a href=#everyday>The Everyday Backpack</a></li>
+    <li><a href=#footer>Footer</a></li>
+  </ul>
+`;
+
+const header = document.querySelector(".siteheader");
+
+const newNavBar = document.createElement("nav");
+const navList = document.createElement("ul");
+navList.classList.add("sitenavbar");
+navList.innerHTML = navBar;
+newNavBar.append(navList);
+
+header.insertAdjacentElement("afterend", newNavBar);
--- 06_04/script.js
+++ 06_04/script.js
@@ -3,7 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-var color = "purple";
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
@@ -11,11 +11,14 @@ document.querySelector(".left .color-value").innerHTML = color;
 color = "skyblue";
 
 function headingColor() {
-  color = "blue";
-  document.querySelector(".title").style.color = color;
+  let titleColor = "blue";
+  document.querySelector(".title").style.color = titleColor;
+  console.log("inside", titleColor);
 }
 
 headingColor();
+
+console.log("outside", titleColor);
 
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
