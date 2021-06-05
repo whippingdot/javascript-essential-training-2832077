0a73f41a6714e708702e95a8089defb93871e049
Sanjaay R <64354499+whippingdot@users.noreply.github.com>
2021-06-05 09:42:28 +0530

    studied a little bit

--- 08_08/script.js
+++ 08_08/script.js
@@ -8,15 +8,23 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const tipCalculator = (sum, percentage) => {
+const formatter = (locale, currency, value) => {
+  let formattedValue = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+  return formattedValue;
+};
+
+const tipCalculator = (sum, percentage, locale, currency) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
   console.log(`
-      Sum before tip: ${sum}
+      Sum before tip: ${formatter(locale, currency, sum)}
       Tip percentage: ${percentage}%
-      Tip:            ${tip.toFixed(2)}
-      Total:          ${total.toFixed(2)}
+      Tip:            ${formatter(locale, currency, tip.toFixed(2))}
+      Total:          ${formatter(locale, currency, total.toFixed(2))}
     `);
 };
 
-tipCalculator(29.95, 18);
+tipCalculator(29.95, 18, "de-DE", "EUR");
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,62 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// NORMAL FUNCTION DECLARATION:
+
+// function createList() {
+//   console.log("Before editing the document");
+
+//   const main = document.querySelector("main");
+//   const newUL = document.createElement("ul");
+
+//   newUL.innerHTML = `
+//   <li>Contact Me</li>
+//   <li>Talk To Me</li>
+//   <li>Chat With Me</li>
+//   `;
+
+//   main.append(newUL);
+
+//   console.log("After editing the document");
+// }
+
+// FUNCTION EXPRESSION DECLARATION:
+
+// const createList = function () {
+//   console.log("Before editing the document");
+
+//   const main = document.querySelector("main");
+//   const newUL = document.createElement("ul");
+
+//   newUL.innerHTML = `
+//   <li>Contact Me Expression</li>
+//   <li>Talk To Me Expression</li>
+//   <li>Chat With Me Expression</li>
+//   `;
+
+//   main.append(newUL);
+
+//   console.log("After editing the document");
+// };
+
+// ARROW FUNCTION DECLARATION:
+
+// const createList = () => {
+//   console.log("Before editing the document");
+
+//   const main = document.querySelector("main");
+//   const newUL = document.createElement("ul");
+
+//   newUL.innerHTML = `
+//   <li>Contact Me Expression</li>
+//   <li>Talk To Me Expression</li>
+//   <li>Chat With Me Expression</li>
+//   `;
+
+//   main.append(newUL);
+
+//   console.log("After editing the document");
+// };
+
+createList();
